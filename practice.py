c41c3d62bb086d9f2d82a4eb980d28a5318e1960
Kaybhee <abdulkabirbadru@gmail.com>
2023-03-26 18:48:27 +0100

    all

---
+++ practice.py
@@ -0,0 +1,13 @@
+def num():
+    import random
+    name = [0,1,2,3,4,5,6,7]
+    random_name = random.choice(name)
+    return num
+def numbers(name)<F11><F11><F11><F11><F11><F11><F11><F11><F11><F11><F11><F11><F11><F11><F11><F11><F11><F11><F11><F11><F11><F11><F11><F11><F11><F11><F11><F11><F11><F11><F11><F11><F11><F11><F11><F11><F11><F11><F11><F11><F11><F11><F11><F11><F11<F11<F11<F11<F11<F11<F11<F11<F11<F11<F11><F11><F11><F11><F11><F11><F11><F11><F11><F11><F11><F11><F11><F1<F11><F11><F11><F11><F11><F11><F11><F11><F11><F11><F11><F11><F11><F11><F11><F11><F11<F11<F11><F11<F11<F11<F11<F1<F11<F11><F11<F11<F11<F11<F11<F11<F11><F11><F11><F11><F11><F11><F11><F11><F11><F11><F11><F11>1><F11>:
+    pick_no = []
+    pick_no.append(num())
+    return numbers
+user_num = numbers(numbers(num))
+print(user_num)
+
+
